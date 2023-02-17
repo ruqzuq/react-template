1f3be6036118e6bda278955393e3ff3f12c3e137
Daniel <82470542+ruqzuq@users.noreply.github.com>
2023-02-18 00:09:11 +0100

    chore: remove devTool: 'source-map'

--- webpack.config.js
+++ webpack.config.js
@@ -3,40 +3,39 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = {
-	entry: path.resolve(__dirname, './src/index.js'),
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx)$/,
-				exclude: /node_modules/,
-				use: ['babel-loader'],
-			},
-			{
-				test: /\.css$/i,
-				use: ['style-loader', 'css-loader'],
-			},
-			{
-				test: /\.svg/,
-				use: ['svg-url-loader'],
-			},
-		],
-	},
-	resolve: {
-		extensions: ['*', '.js', '.jsx', '.css', '.svg', '.html'],
-		fallback: { path: require.resolve('path-browserify') },
-	},
-	output: {
-		path: path.resolve(__dirname, './public'),
-		filename: 'index_bundle.js',
-	},
-	plugins: [
-		new webpack.HotModuleReplacementPlugin(),
-		new HtmlWebpackPlugin({ inject: true, template: './src/index.html' }),
-	],
-	mode: 'development',
-	devtool: 'source-map',
-	devServer: {
-		static: path.resolve(__dirname, './public'),
-		hot: true,
-	},
+  entry: path.resolve(__dirname, './src/index.js'),
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.svg/,
+        use: ['svg-url-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx', '.css', '.svg', '.html'],
+    fallback: { path: require.resolve('path-browserify') },
+  },
+  output: {
+    path: path.resolve(__dirname, './public'),
+    filename: 'index_bundle.js',
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({ inject: true, template: './src/index.html' }),
+  ],
+  mode: 'development',
+  devServer: {
+    static: path.resolve(__dirname, './public'),
+    hot: true,
+  },
 };
